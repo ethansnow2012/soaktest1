d2b0d6b560ec38f704d40e50cdfbc66e6e74d8d5
Kao Ethan <ethansnow2012@gmail.com>
2021-04-06 15:34:45 +0800

    --

--- src/main.js
+++ src/main.js
@@ -4,20 +4,7 @@ import router from './router/index'
 
 
 let app = createApp(App)
-app.directive('click-outside', {
-    beforeMount(el, binding, vnode) {
-        el.clickOutsideEvent = function(event) {
-            if (!(el === event.target || el.contains(event.target))) {
-                //console.log(event.target)
-                binding.value(event, el);
-            }
-        };
-        document.body.addEventListener('click', el.clickOutsideEvent);
-    },
-    unmounted(el) {
-        document.body.removeEventListener('click', el.clickOutsideEvent);
-    }
-})
+
 
 
 app.use(router).mount('#app')
